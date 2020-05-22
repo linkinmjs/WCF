0c1d1a00ed6a213f6325dfe03ff426cdfe4b75d6
Mauri <mauricio.stampella@25watts.com.ar>
2020-05-22 11:20:32 -0300

    Primer Commit

---
+++ PrimerServicioWCF/PrimerServicioWCF.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerServicioWCF
+{
+    public class PrimerServicioWCF : IPrimerServicioWCF
+    {
+        public string ObtenerMensaje(string name) 
+        {
+            return "Hola desde DotNetUy " + name + "!";
+        }
+    }
+}
